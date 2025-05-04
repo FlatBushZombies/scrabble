cd12541bfa45e035fa2099343eaa0dd0049fd069
BrianMakanjira <makanjirabrian@gmail.com>
2025-05-04 15:34:52 +0200

    middleware setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Add this if you're using the Pages Router
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  }
 };
 
 export default nextConfig;
